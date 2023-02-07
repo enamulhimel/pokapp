6e49d50900a90f30ae011e4feaabea535b33a4a9
enamulhimel <mhasan173013@bscse.uiu.ac.bd>
2023-02-07 18:50:57 +0600

    api connected

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,8 @@ import Head from 'next/Head';
 import Layout from 'components/Layout';
 
 
-export default function Home() {
+export default function Home({pokemon}) {
+  console.log(pokemon);
   return (
     <Layout title="NextJS pokapp">
       <h1 className='text-4xl mb-8 text-center'>NextJS pokapp</h1>
@@ -14,10 +15,18 @@ export async function getStaticProps(context){
   try{
     const res = await fetch('https://pokeapi.co/api/v2/pokemon?limit=150');
     const {results} = await res.json();
+    const pokemon = results.map((result,index)=>{
+    const paddedIndex = ("00"+(index+1)).slice(-3);  //001
+      const image = `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${paddedIndex}.png`
+      return{
+        ...result,
+        image,
+      };
+    });
+    return{
+      props:{pokemon},
+    };
   }catch(err){
     console.error(err);
-  }
-  return{
-    props:{}
   }
 }
