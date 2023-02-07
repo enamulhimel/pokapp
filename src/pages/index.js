d819f06a764a22065de6767120695ecd5d943dcb
enamulhimel <mhasan173013@bscse.uiu.ac.bd>
2023-02-07 20:39:57 +0600

    image lodded

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,32 +1,46 @@
-import Head from 'next/Head';
+import Link from 'next/Link';
 import Layout from 'components/Layout';
-
-
-export default function Home({pokemon}) {
-  console.log(pokemon);
-  return (
-    <Layout title="NextJS pokapp">
-      <h1 className='text-4xl mb-8 text-center'>NextJS pokapp</h1>
-    </Layout>
-  );
+export default function Home({ pokemon }) {
+    return (
+        <Layout title="NextJS Pokedex">
+            <h1 className="text-4xl mb-8 text-center ">The Nextjs Pokedex</h1>
+            <ul>
+                {pokemon.map((pokeman, index) => (
+                    <li key={index}>
+                        <Link href={`/pokemon?id=${index + 1}`}>
+                            <picture className="border p-4 border-grey my-2 hover:shadow-md capitalize flex items-center text-lg bg-gray-200 rounded-md">
+                                <img
+                                    src={pokeman.image}
+                                    alt={pokeman.name}
+                                    className="w-20 h-20 mr-3"
+                                />
+                                <span className="mr-2 font-bold">
+                                    {index + 1}.
+                                </span>
+                                {pokeman.name}
+                            </picture>
+                        </Link>
+                    </li>
+                ))}
+            </ul>
+        </Layout>
+    );
 }
 
-export async function getStaticProps(context){
-  try{
-    const res = await fetch('https://pokeapi.co/api/v2/pokemon?limit=150');
-    const {results} = await res.json();
-    const pokemon = results.map((result,index)=>{
-    const paddedIndex = ("00"+(index+1)).slice(-3);  //001
-      const image = `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${paddedIndex}.png`
-      return{
-        ...result,
-        image,
-      };
-    });
-    return{
-      props:{pokemon},
-    };
-  }catch(err){
-    console.error(err);
-  }
+export async function getStaticProps(context) {
+    try {
+        const res = await fetch('https://pokeapi.co/api/v2/pokemon?limit=150');
+        const { results } = await res.json();
+        const pokemon = results.map((pokeman, index) => {
+            const paddedId = ('00' + (index + 1)).slice(-3);
+
+            const image = `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${paddedId}.png`;
+            return { ...pokeman, image };
+        });
+        return {
+            props: { pokemon },
+        };
+    } catch (err) {
+        console.error(err);
+    }
 }
